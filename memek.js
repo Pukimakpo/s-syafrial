d7bdd86a101f0f49cd01fac31566b3fdc75708a7
Pukimakpo <hamjikolo@gmail.com>
2025-07-22 22:08:46 -0700

    Create memek.js
    Signed-off-by: Pukimakpo <hamjikolo@gmail.com>

---
+++ memek.js
@@ -0,0 +1,13 @@
+{
+  "users": [
+    {
+      "user": "admin",
+      "pass": "admin",
+      "isAdmin": true,
+      "allowed": true,
+      "max_conc": 999,
+      "time_limit": 999,
+      "expiredAt": 9999999999999
+    }
+  ]
+}
