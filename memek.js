f754ccb4f31f5565f3d2e067130e6d0f27937d65
Pukimakpo <hamjikolo@gmail.com>
2025-07-22 23:00:23 -0700

    Update and rename memek.json to memek.js
    Signed-off-by: Pukimakpo <hamjikolo@gmail.com>

---
+++ memek.js
@@ -0,0 +1,14 @@
+module.exports = {
+  users: [
+    {
+      user: "1",
+      pass: "1",
+      isAdmin: true,
+      allowed: true,
+      conc: 10,
+      durasi: 60,
+      expired: "2099-12-31"
+    }
+  ],
+  attacks: []
+};
