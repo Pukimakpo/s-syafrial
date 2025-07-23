756e0a182b2b7a5f1a0316d4214296bc17371b97
Pukimakpo <hamjikolo@gmail.com>
2025-07-23 01:51:34 -0700

    Update memek.js
    Signed-off-by: Pukimakpo <hamjikolo@gmail.com>

--- memek.js
+++ memek.js
@@ -1,14 +1,11 @@
-module.exports = {
-  users: [
-    {
-      user: "1",
-      pass: "1",
-      isAdmin: true,
-      allowed: true,
-      conc: 10,
-      durasi: 60,
-      expired: "2099-12-31"
-    }
-  ],
-  attacks: []
-};
+[
+  {
+    "user": "1",
+    "pass": "1",
+    "isAdmin": true,
+    "allowed": true,
+    "limit": 1231235,
+    "time": 6123123123123120,
+    "expires": "9999-12-31"
+  }
+]
